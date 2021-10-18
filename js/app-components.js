9a6a0b0059ddda9adfb154d7d7ee76aa8e1ad790
jmotl <jmotl96@gmail.com>
2021-10-18 00:09:17 -0500

    night one

---
+++ js/app-components.js
@@ -0,0 +1,123 @@
+const Library = Vue.component('Library', {
+    // this function is run AFTER the props have been passed in
+    data() {
+        return {
+            library: [
+                new Book('Interaction Design', 200),
+                new Movie('Paw Patrol!', 78),
+                new Movie('Harriet', 122),
+                new Book('Brown Bear, Brown Bear', 0),
+            ]
+        }
+    },
+
+    template: `
+      <div class="card-columns">
+        <library-item v-for="item in library" :item="item"></library-item>
+<!--          <div class="card" v-for="item in library">-->
+<!--            <book v-if="item.constructor.name === 'Book'" :item="item"></book>-->
+<!--            <movie v-else-if="item.constructor.name === 'Movie'" :item="item"></movie>-->
+<!--          </div>-->
+      </div>
+    `
+})
+
+const LibraryItemComponent = Vue.component('LibraryItem', {
+    // values/bindings passed to this component
+    props: {
+        item: LibraryItem
+    },
+
+    computed: {
+      typeOfItem(){
+          return this.item.constructor.name;
+      }
+    },
+
+    // the view
+    template: `
+            <div class="card" :class="item.isAvailable() ? 'border-success' : 'border-warning'">
+<!--                <h3 class="card-title">{{item.title}}</h3>-->
+<!--                <p class="card-text" v-if="item.constructor.name == 'Book'">Pages: {{item.pages}}</p>-->
+<!--                <p class="card-text" v-if="item.runningTime">Running Time: {{item.runningTime}}</p>-->
+            <div class="card-body">
+              <component :is="typeOfItem" :item="item"></component>
+            </div>
+            <div class="card-footer">
+              <button @click="item.checkOut()" class="btn btn-secondary">Check Out</button>
+              <button @click="item.checkIn()" class="btn btn-secondary">Check In</button>
+            </div>
+            </div>
+        `,
+});
+
+
+const Cart = Vue.component('cart',{
+    data() {
+        return {
+            cart: [
+                new Book('Interaction Design', 200),
+                new Movie('Paw Patrol!', 78),
+            ]
+        }
+    },
+    template:`
+      <div class="card-columns">
+      <cart-item v-for="item in cart" :item="item"></cart-item>
+      </div>
+    `
+
+})
+
+const CartItemComponent = Vue.component('cartItem', {
+    // values/bindings passed to this component
+    props: {
+        item: LibraryItem
+    },
+
+    computed: {
+        typeOfItem(){
+            return this.item.constructor.name;
+        }
+    },
+
+    // the view
+    template: `
+            <div class="card" :class="item.isAvailable() ? 'border-success' : 'border-warning'">
+<!--                <h3 class="card-title">{{item.title}}</h3>-->
+<!--                <p class="card-text" v-if="item.constructor.name == 'Book'">Pages: {{item.pages}}</p>-->
+<!--                <p class="card-text" v-if="item.runningTime">Running Time: {{item.runningTime}}</p>-->
+            <div class="card-body">
+              <component :is="typeOfItem" :item="item"></component>
+            </div>
+            <div class="card-footer">
+              <button @click="item.checkOut()" class="btn btn-primary">delete</button>
+            </div>
+            </div>
+        `,
+});
+
+
+const BookComponent = Vue.component('Book', {
+    // inherit props/methods
+    extends: LibraryItemComponent,
+
+    template: `
+            <div class="book">
+                <h3 class="card-title">{{item.title}}</h3>
+                <p class="card-text">Pages: {{item.pages}}</p>
+            </div>
+        `,
+});
+
+const MovieComponent = Vue.component('Movie', {
+    // inherit props/methods
+    extends: LibraryItemComponent,
+
+    template: `
+            <div class="movie">
+                <h3 class="card-title">{{item.title}}</h3>
+                <p class="card-text">Running Time: {{item.runningTime}}</p>
+            </div>
+        `,
+});
---
+++ js/app-models.js
@@ -0,0 +1,63 @@
+// The 'Model' is responsible for managing the data of the application.
+// You can define your models in the 'data' section of each Vue component
+// or define them separately if they might be used by multiple components.
+
+// Models are usually prototypes (similar to classes if you are familiar with those)
+function LibraryItem(title){
+    // list of possible values (enum)
+    const STATUSES = {CHECKED_OUT: 'out', CHECKED_IN: 'in', LOST: 'lost'}
+
+    this.title = title || 'Default Title';
+    this.id = Math.floor(Math.random() * 10e16);
+    this.status = STATUSES.CHECKED_IN;
+
+    // methods
+    this.checkIn = function(){
+        this.status = STATUSES.CHECKED_IN;
+    }
+
+    this.checkOut = function(){
+        this.status = STATUSES.CHECKED_OUT;
+
+    }
+
+    this.isAvailable = function(){
+        return this.status === STATUSES.CHECKED_IN;
+    }
+
+}
+
+// let book = new LibraryItem('Interaction Design', 200);
+// let movie = new LibraryItem('Paw Patrol!', null, 78);
+//
+// console.log(book);
+
+
+function Book(title, pages){
+    //LibraryItem.apply(this, [title])
+    LibraryItem.call(this, title)
+
+    this.pages = pages;
+}
+// sets the prototype
+Book.prototype = Object.create(LibraryItem.prototype);
+// resets the constructor
+Book.prototype.constructor = Book;
+
+// same as above using class syntax
+class Movie extends LibraryItem{
+    runningTime;
+
+    constructor(title, runningTime){
+        super(title);
+
+        this.runningTime = runningTime;
+    }
+}
+
+//LibraryItem.prototype.catalog = '201.34';
+
+let book = new Book('Interaction Design', 200);
+let movie = new Movie('Paw Patrol!', 78);
+
+console.log(book.constructor.name, book, movie.constructor.name, movie);
